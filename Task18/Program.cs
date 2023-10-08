7d9e78e46b762fba3c1f33fc5e80f36b8f2d1503
Pavel <grigorov_ps@mail.ru>
2023-10-08 15:49:57 +0300

    Создали проект Task18 (определяет дипрозон координат по указаной четверти)

---
+++ Task18/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишем программу, которая
+// 1. по заданному номеру четверти,
+// 2. показывает диапозон координат точек в этой четверти (X и Y)
+//
+string Quarter(int num)
+{
+    if(num == 1) return "X > 0 , Y > 0 ";
+    if(num == 2) return "X < 0 , Y > 0 ";
+    if(num == 3) return "X < 0 , Y < 0 ";
+    if(num == 4) return "X > 0 , Y < 0 ";
+    else
+    {
+    return "Четверти с таким номером нет.";
+    }
+}
+
+Console.WriteLine("Введите номер четверти от 1 до 4: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string result = Quarter(quarter);
+Console.WriteLine(result);
+
