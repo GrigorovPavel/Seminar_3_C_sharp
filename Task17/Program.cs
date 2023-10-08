4f7e8bd98d1cd9837547cdacf133bc8ef53837a6
Pavel <grigorov_ps@mail.ru>
2023-10-08 15:06:59 +0300

    Создати проект Task17 (определяет четверть квадрата по координатам X и Y

--- Task17/Program.cs
+++ Task17/Program.cs
@@ -5,21 +5,20 @@
 
 int Quarter(int x, int y)  // Метод определяет "четверть" квадрата
 {
-    if(x > 0 && y > 0) return 1;
-    if(x < 0 && y > 0) return 2;
-    if(x < 0 && y < 0) return 3;
-    if(x > 0 && y < 0) return 4;
+    if (x > 0 && y > 0) return 1;
+    if (x < 0 && y > 0) return 2;
+    if (x < 0 && y < 0) return 3;
+    if (x > 0 && y < 0) return 4;
     return 0;
 }
 
 Console.WriteLine("Введите координату X, больше 0");
 int xСoordinate = Convert.ToInt32(Console.ReadLine());
-
 Console.WriteLine("Введите координату Y, больше 0");
 int yСoordinate = Convert.ToInt32(Console.ReadLine());
 
-int quarter = Quarter(xСoordinate, yСoordinate);
-if(xСoordinate ==0 || yСoordinate ==0)
+int quarter = Quarter(xСoordinate, yСoordinate); // присваиваем переменной, результат метода
+if (xСoordinate == 0 || yСoordinate == 0)
 {
     Console.WriteLine("Некорректный ввод.");
 }
