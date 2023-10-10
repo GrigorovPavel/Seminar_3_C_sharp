2713f45b28d9a2cf57bce5b975892be0a3870a23
Pavel <grigorov_ps@mail.ru>
2023-10-10 22:21:34 +0300

    Создали программу Task22 (выдает таблицу квадратов чисел от 1 до N)

---
+++ Task22/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу, которая 
+// 1. Принимает на вход число (N) и
+// 2. выдает таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+TableSquare(num);
+
+void TableSquare(int number)
+{
+    int count = 1;
+    while(count <= number)
+    {
+        Console.WriteLine($"{count, 3} -> {count * count, 5}");
+        count++;
+    }
+}
